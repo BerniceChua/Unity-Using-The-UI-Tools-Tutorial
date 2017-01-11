a10fe250facaefc9fa7acab061dbc7e38427d352
BerniceChua <Bernice.Chua.415@gmail.com>
2017-01-10 22:32:50 -0800

    Add the .zip file with the UI Demo Art & UI Demo Textures.

---
+++ Assets/Scripts/ObjectPoolerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolerScript : MonoBehaviour {
+
+    public static ObjectPoolerScript current;
+    public GameObject pooledGameObject;
+    public int pooledAmount = 20;
+    public bool willGrow = true;
+
+    List<GameObject> pooledGameObjects;
+
+    void Awake() {
+        current = this;
+    }
+
+    // Use this for initialization
+    void Start() {
+        pooledGameObjects = new List<GameObject>();
+
+        for (int i = 0; i < pooledAmount; i++) {
+            GameObject obj = (GameObject)Instantiate(pooledGameObject);
+            obj.SetActive(false);
+            pooledGameObjects.Add(obj);
+        }
+    }
+
+    public GameObject GetPooledGameObject() {
+        for (int i = 0; i < pooledGameObjects.Count; i++) {
+            if (!pooledGameObjects[i].activeInHierarchy) {
+                return pooledGameObjects[i];
+            }
+        }
+
+        if (willGrow) {
+            GameObject obj = (GameObject)Instantiate(pooledGameObject);
+            pooledGameObjects.Add(obj);
+            return obj;
+        }
+
+        return null;
+    }
+
+}
